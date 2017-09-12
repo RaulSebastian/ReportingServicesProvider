517dc3be3e96db8ea1e843beea76fa3ee4470d2c
Raul Sebastian <raulschnelzer@gmail.com>
2017-09-12 07:56:46 +0200

    updated dto mapping

--- source/ReportingServicesProvider.ServiceInterface/Mappers.cs
+++ source/ReportingServicesProvider.ServiceInterface/Mappers.cs
@@ -1,14 +1,15 @@
-﻿using ServiceStack;
+﻿using System;
 using ReportingServicesProvider.ServiceModel.Requests.Reports;
 using ReportingServicesProvider.ServiceModel.Requests.Servers;
 using ReportingServicesProvider.ServiceModel.Types;
+using ServiceStack;
 
 namespace ReportingServicesProvider.ServiceInterface
 {
     public static class Mappers
     {
         public static Server ToDto(this PostServer from)
-            => new Server().PopulateWith(from);
+            => new Server {Created = DateTime.Now}.PopulateWith(from);
 
         public static Server ToDto(this UpdateServerById from)
             => new Server().PopulateWith(from);
@@ -20,7 +21,7 @@ namespace ReportingServicesProvider.ServiceInterface
             => new Server().PopulateWith(from);
 
         public static Report ToDto(this PostReport from)
-            => new Report {Server = from.SId}.PopulateWith(from);
+            => new Report {Server = from.SId, Created = DateTime.Now}.PopulateWith(from);
 
         public static Report ToDto(this UpdateReportById from)
             => new Report {Server = from.SId}.PopulateWith(from);
